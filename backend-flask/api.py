22d51ef37b88bbcbedf8bd1fce8eb632d7022ccf
Tony Doan <ynotdoan@gmail.com>
2023-04-02 20:58:15 -0700

    Added get and post api functions for database

--- backend-flask/api.py
+++ backend-flask/api.py
@@ -4,6 +4,7 @@ from flask import request, jsonify
 from contextlib import closing
 import hashlib
 import sqlite3
+from datetime import datetime 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = config.secret_key
@@ -57,7 +58,37 @@ def check_password(pwd, stored_pwd):
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
-    pass
+    with closing(sqlite3.connect(config.database)) as conn:
+        with closing(conn.cursor()) as cursor:
+            data = cursor.execute(
+                'SELECT TOP 100 * FROM BinSortData'
+            )
+    
+    return jsonify({'status': 'success',
+                    'code': 200,
+                    'data': data,
+                    'message': 'successfully retrieved last 100 data entries.'})
+
+@app.route('/api/data', methods=['POST'])
+def post_data():
+    date = datetime.now()
+    part_img = 0
+    damages = []
+    system_decision = 0
+    sorter_decision = 0
+    sorter_id = 0
+
+    with closing(sqlite3.connect(config.database)) as conn:
+        with closing(conn.cursor()) as cursor:
+            cursor.execute(
+                'INSERT INTO BinSortData (Date, PartImage, Damages, SystemSecision, SorterDecision, SorterId) VALUES (?, ?, ?, ?, ?, ?)', 
+                (date, part_img, damages, system_decision, sorter_decision, sorter_id)
+            )
+
+    return jsonify({'status': 'success',
+                    'code': 200,
+                    'data': None,
+                    'message': 'successfully posted to database.'})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
