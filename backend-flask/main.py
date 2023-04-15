6c24a77a5ece1a5c1902219710161c1a57f7fe5f
lortiz-gonzalez <lortiz-gonzalez@ucmerced.edu>
2023-04-15 11:44:46 -0700

    .

--- backend-flask/main.py
+++ backend-flask/main.py
@@ -19,6 +19,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = config.secret_key
 cors = CORS(app)
 
+
 def openConnection(_dbFile):
     try:
         conn = sqlite3.connect(_dbFile)
@@ -36,40 +37,43 @@ def closeConnection(_conn, _dbFile):
         print(e)
 
 
-def insertBinaryToTable(conn,filename):
+def insertBinaryToTable(conn,filename,empid):
     cur = conn.cursor()
     with open(filename, 'rb') as file:
         blob = file.read()
         #print(blob)
-    sql = """INSERT INTO Images (image_blob,defects,sys_verdict,emp_verdict,emp_id)
-            VALUES (?,?,?,?,?);"""
-    args = [blob, 3,'Good','Bad',3]
+    sql = """INSERT INTO Images (image_blob, emp_id, sys_verdict, emp_verdict)
+            VALUES (?,?,?,?);"""
+    args = [blob, empid,'Good','Good']
     cur.execute(sql,args)
     conn.commit()
+    
 
 def outputImage(conn, imgID):
     cur = conn.cursor()
     sql = """SELECT * FROM Images WHERE image_id = ?"""
     cur.execute(sql,(imgID,))
-    res = cur.fetchone()[2] # stores the third element returned(BLOB number)
+    res = cur.fetchone()[2]  # stores the third element returned(BLOB number)
     #print(res)
-    path = 'images/test{0}.png'.format(imgID)
+    path = 'images/test{0}.png'.format(imgID) # Location where the image will be returned to
     with open(path, 'wb') as file:
         file.write(res)
         file.close()
 
 
 def main():
-    database = r'database.db'
+    database = r'Bin_Sort_db.db'
     conn = openConnection(database)
-
+    empid=0
     #---- Trying New Method to store/retrieve images ------------
     for images in os.listdir(path):
         if (images.endswith(".PNG")):
-            insertBinaryToTable(conn,path+images)
+            if empid < 5:
+                empid = empid + 1
+            insertBinaryToTable(conn,path + images, empid)
             print('image added')
     
-    outputImage(conn,5)
+    #outputImage(conn,5)
 
     closeConnection(conn, database)
 
