36b3df8807372cd3e34130791d2a22b31b10a3af
Renato Millan <91162242+GodHVoid@users.noreply.github.com>
2023-04-23 11:29:57 -0700

    Add files via upload

--- CV_video.py
+++ CV_video.py
@@ -5,25 +5,22 @@ import os
 import yolov5
 # Playing video from file:
 cap = cv2.VideoCapture('video1.mp4')
-cap.set(cv2.CAP_PROP_FPS, 15)
+cap.set(cv2.CAP_PROP_FPS, 30)
 
 currentFrame = 0;
 count =0;
-model = torch.hub.load('yolov5', 'custom', path='yolov5s.pt',source='local',force_reload=True,device= 'cpu' )
-#model = yolov5.load('best.pt')
-#model = torch.hub.load('yolov5','yolov5s')
+model = torch.hub.load( 'yolov5','custom', path='yolov5s.pt',source='local',force_reload=True,device= 'cpu' )
 model.conf = 0.4
 model.iou = 0.45
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
-    
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     result = model(frame, size=640)
     frame = result.render()[0]
-    
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
     result.print()
-    #result.show()
-    #cv2.imshow('model', result.render()[0]) 
+
     cv2.imshow('test', frame)
     
     if cv2.waitKey(1) == ord('q'):
