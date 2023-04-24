1a278c27d0a9cd9c1422f8b2568d1f743b0c4c3e
Tony Doan <ynotdoan@gmail.com>
2023-04-24 10:14:35 -0700

    Camera calls pt model with weights

--- backend-flask/camera.py
+++ backend-flask/camera.py
@@ -4,28 +4,29 @@ import cv2
 import torch
 
 
-# model = torch.hub.load('custom', 
-                    #    path='best.pt',
-                    #    source='local',
-                    #    force_reload=True, 
-                    #    device= 'cpu' )
-
+model = torch.hub.load( 'yolov5','custom', path='best.pt',source='local',force_reload=True,device= 'cpu' )
+model.conf = 0.4
+model.iou = 0.45
 
 class VideoCamera(object):
     def __init__(self):
-        path = './sides09.mp4'
+        path = './sides08.mp4'
         self.video = cv2.VideoCapture(path)
-        self.video.set(cv2.CAP_PROP_FPS, 30)
+        self.video.set(cv2.CAP_PROP_FPS, 60)
 
     def __del__(self):
         self.video.release()
 
     def get_frame(self):
         ret, frame = self.video.read()
-        # result = model(frame, size=640)
-        # frame = result.render()[0]
+
 
         if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            result = model(frame, size=640)
+            frame = result.render()[0]
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            result.print()
             ret, frame = cv2.imencode('.png', frame)
             return frame.tobytes()
         return None
