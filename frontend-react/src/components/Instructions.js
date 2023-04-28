73ab5c48315d5862b3b4094684954504e346e54d
Tony Doan <ynotdoan@gmail.com>
2023-04-27 19:03:44 -0700

    Updated instruction window css

--- frontend-react/src/components/Instructions.js
+++ frontend-react/src/components/Instructions.js
@@ -1,20 +1,29 @@
 import React from "react";
+import "../styles/Popup.css";
 
 function Instructions() {
   return (
     <div className="Instructions">
       <div className="LoginInstructions">
-        Login: <br />
-        Log in using your username (first initial and last name) and your password.
+        <h1>
+          Login: 
+        </h1>
+        <h2>
+          Log in using your username (first initial and last name) and your password.
+        </h2>
       </div>
       <br />
       <div className="LiveSortInstructions">
-        LiveSort: <br />
+        <h1>
+          LiveSort: 
+        </h1>
+        <h2>
         The stationary camera will detect the bin parts and determine whether it is 
         usable or to be destroyed. It will list the damages on the right side of the 
         screen and an image of the bin part on the left. <br />
         When an image is displayed, the sorter will then make their decision on whether 
-        the part should be usable (green button) or destroyed (red button).
+        the part should be usable (green button) or destroyed (red button).          
+        </h2>
       </div>
     </div>
   );
