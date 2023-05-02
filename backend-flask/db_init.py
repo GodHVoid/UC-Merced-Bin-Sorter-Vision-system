5ebaeb549605fb09d3bc4f0f712a49a6fde0fdea
Tony Doan <ynotdoan@gmail.com>
2023-05-02 14:32:12 -0700

    Removed overrides table

--- backend-flask/db_init.py
+++ backend-flask/db_init.py
@@ -4,27 +4,9 @@ from auth import hash_password
 import sqlite3
 from sqlite3 import Error
 import random
-import base64
 
 def dummy_data(cur):
-    users = [
-        ('Tony', 'Doan', 'tdoan', '123Doan', 0),
-        ('Renato', 'Millan', 'rmillan', '123Millan', 0),
-        ('Lucas', 'Ortiz-Gonzales', 'lortizgonzales', '123Ortizgonzales', 0),
-        ('Rui', 'Pan', 'rpan', '123Pan', 0),
-        ('Justus', 'Sasse', 'jsasse', '123Justus', 1),
-        ('Bruce', 'Bates', 'bbates', '123Bruce', 1)
-    ]
-    
-    for user in users:
-        cur.execute(
-            'INSERT INTO Users (firstname, lastname, username, password, is_trainer) \
-                VALUES (?, ?, ?, ?, ?)',
-            (user[0], user[1], user[2], hash_password(user[3]), user[4],)
-        )
-    print('Successfully inserted users into Users table.')
-
-    part_name = ['metals', 'plains', 'tops', 'base']
+    part_name = ['Plains', 'Metals', 'Tops', 'Bases']
     decisions = ['false', 'true']
     for i in range(1,51):
         with open('./imgs/'+str((i%10))+'.png', 'rb') as f:
@@ -36,13 +18,6 @@ def dummy_data(cur):
             (random.choice(part_name), img, random.randint(1,6), random.choice(decisions), random.choice(decisions))
         )
     print('Successfully inserted images into Images table.')
-
-    inventory = [('Plains', 0), ('Tops', 0), ('Metals', 0), ('Base', 0)]
-    for i in inventory:
-        cur.execute(
-            'INSERT INTO Inventory (part_type, count) VALUES (?,?)',
-            (i)
-        )
 
     return 'Successfully pushed all dummy data.'
 
@@ -63,7 +38,31 @@ if __name__ == '__main__':
         conn.commit()
         print('Tables created')
 
-        print(dummy_data(cur))
+        users = [
+            ('Tony', 'Doan', 'tdoan', '123Doan', 0),
+            ('Renato', 'Millan', 'rmillan', '123Millan', 0),
+            ('Lucas', 'Ortiz-Gonzales', 'lortizgonzales', '123Ortizgonzales', 0),
+            ('Rui', 'Pan', 'rpan', '123Pan', 0),
+            ('Justus', 'Sasse', 'jsasse', '123Justus', 1),
+            ('Bruce', 'Bates', 'bbates', '123Bruce', 1)
+        ]
+    
+        for user in users:
+            cur.execute(
+                'INSERT INTO Users (firstname, lastname, username, password, is_trainer) \
+                    VALUES (?, ?, ?, ?, ?)',
+                (user[0], user[1], user[2], hash_password(user[3]), user[4],)
+            )
+        print('Successfully inserted users into Users table.')
+
+        inventory = [('Plains', 0), ('Tops', 0), ('Metals', 0), ('Bases', 0)]
+        for i in inventory:
+            cur.execute(
+            'INSERT INTO Inventory (part_type, count) VALUES (?,?)',
+            (i)
+        )
+
+        # print(dummy_data(cur))
         conn.commit()
 
     except Error as e:
--- backend-flask/schema.sql
+++ backend-flask/schema.sql
@@ -1,8 +1,6 @@
 DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Images;
-DROP TABLE IF EXISTS Overrides;
-DROP TABLE IF EXISTS Part_Conditions;
-DROP TABLE IF EXISTS DefectsList;
+DROP TABLE IF EXISTS Damages;
 DROP TABLE IF EXISTS Inventory;
 
 -- Creates Tables
@@ -29,41 +27,14 @@ CREATE TABLE Images (
     override    BOOLEAN
 );
 
-CREATE TABLE Part_Conditions (
+CREATE TABLE Damages (
     p_image_id    INTEGER  REFERENCES Images (image_id) 
                            UNIQUE
                            NOT NULL,
-    p_part_type   STRING   NOT NULL,
-    date          DATETIME NOT NULL
-                           DEFAULT (datetime('now', 'localtime') ),
-    verdict       STRING   NOT NULL,
     Corner_damage INTEGER,
     Edge_damage   INTEGER,
-    Logo_repair,
-    Cleat_damage  INTEGER,
-    Clear_repair
-);
-
-CREATE TABLE Overrides (
-    override_id    INTEGER  PRIMARY KEY AUTOINCREMENT
-                            NOT NULL,
-    date           DATETIME NOT NULL
-                            DEFAULT (datetime('now', 'localtime') ),
-    o_image_id     INTEGER  NOT NULL,
-    trainer_id     INTEGER  NOT NULL,
-    new_condition  STRING   NOT NULL,
-    prev_condition STRING   NOT NULL,
-    Corner_damage  INTEGER,
-    Edge_damage    INTEGER,
-    Logo_repair    INTEGER,
-    Cleat_damage   INTEGER,
-    Clear_repair   INTEGER
-);
-
-CREATE TABLE DefectsList (
-    value       INTEGER PRIMARY KEY AUTOINCREMENT,
-    defect_type STRING  NOT NULL
-                        UNIQUE
+    Logo_repair   INTEGER,
+    Cleat_damage  INTEGER
 );
 
 CREATE TABLE Inventory (
@@ -73,84 +44,12 @@ CREATE TABLE Inventory (
     count     INTEGER
 );
 
------------------------- Triggers --------------------------
--- After initial image is processed, inserts into Part_Conditions with more details on defects present
-CREATE TRIGGER insert_to_Part_Conditions
-         AFTER INSERT
-            ON Images
-BEGIN
-    INSERT INTO Part_Conditions (
-                                    p_image_id,
-                                    p_part_type,
-                                    verdict,
-                                    Corner_damage,
-                                    Edge_damage,
-                                    Logo_repair,
-                                    Cleat_damage,
-                                    Clear_repair
-                                )
-                                VALUES (
-                                    new.image_id,
-                                    new.part_type,
-                                    new.emp_verdict,
-                                    1,
-                                    1,
-                                    1,
-                                    1,
-                                    1
-                                );
-END;
--- When verdict is changed, keeps history of the new verdict and defects
--- Also updates value in Images.override to 1, meaning the employee verdict has been overwritten
-CREATE TRIGGER insert_into_overrides
-         AFTER UPDATE OF emp_verdict
-            ON Images
-BEGIN
-    INSERT INTO Overrides (
-                              o_image_id,
-                              trainer_id,
-                              new_condition,
-                              prev_condition,
-                              Corner_damage,
-                              Edge_damage,
-                              Logo_repair,
-                              Cleat_damage,
-                              Clear_repair
-                          )
-                          VALUES (
-                              new.image_id,
-                              5,
-                              new.emp_verdict,
-                              old.emp_verdict,
-                              0,
-                              0,
-                              0,
-                              0,
-                              0
-                          );
-    UPDATE Images
-       SET override = 1
-     WHERE image_id = new.image_id;
-END;
--- Once an entry is made into the Overrides table, the conditions on an existing image need to be made
-CREATE TRIGGER update_Part_Condtions
-         AFTER INSERT
-            ON Overrides
-BEGIN
-    UPDATE Part_Conditions
-       SET verdict = new.new_condition,
-           Corner_damage = new.Corner_damage,
-           Edge_damage = new.Edge_damage,
-           Logo_repair = new.Logo_repair,
-           Cleat_damage = new.Cleat_damage,
-           Clear_repair = new.Clear_repair
-     WHERE p_image_id = new.o_image_id;
-END;
 
-------------------------------------------------------------
-
--- INSERT INTO Users (firstname,lastname,username,password,is_trainer) VALUES ('Lucas','Ortiz','lortiz', '123Ortiz', 0);
--- INSERT INTO Users (firstname,lastname,username,password,is_trainer) VALUES ('Tony', 'Doan', 'tdoan', '123Doan', 0);
--- INSERT INTO Users (firstname,lastname,username,password,is_trainer) VALUES ('Renato', 'Millan', 'rmillan', '123Renato', 0);
--- INSERT INTO Users (firstname,lastname,username,password,is_trainer) VALUES ('Rui', 'Pan', 'rpan', '123Rui', 0);
--- INSERT INTO Users (firstname,lastname,username,password,is_trainer) VALUES ('Justus', 'Sasses', 'jsasse', '234Justus', 1)
+CREATE TRIGGER update_inventory
+    AFTER INSERT
+        On Images
+BEGIN
+    UPDATE Inventory
+        SET count = count+1
+            WHERE part_type = new.part_type;
+END;
