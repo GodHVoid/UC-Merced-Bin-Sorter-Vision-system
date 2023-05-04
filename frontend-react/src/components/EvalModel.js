6cbda456c8f6a58b258f415637b00b6285b28e87
Tony Doan <ynotdoan@gmail.com>
2023-05-04 12:56:20 -0700

    Updated eval pages

--- frontend-react/src/components/EvalModel.js
+++ frontend-react/src/components/EvalModel.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 function EvalModel() {
   const [confidenceLevel, setConfidenceLevel] = useState("");
   const [verdictDifference, setVerdictDifference] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
 
   useEffect(() => {
     fetch("api")
@@ -14,10 +16,39 @@ function EvalModel() {
       });
   }, []);
 
+  const handleFileInput = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleUpload = () => {
+    if (selectedFile === null) {
+      alert("No file!")
+    }
+  }
+
   return (
     <div>
-      <h2>System Confidence Level: {confidenceLevel}</h2>
-      <h2>System Verdict Difference: {verdictDifference}</h2>
+
+      <div className="toolbar">
+        <Link to={"/data"}>
+          <button className="back-btn">Back</button> 
+        </Link>
+        <h2>Model Evaluation</h2>
+      </div>
+
+      <div className="model-content">
+        <h2>System Confidence Level: {confidenceLevel}</h2>
+        <h2>System Verdict Difference: {verdictDifference}</h2>
+      </div>
+
+      <div className="file-upload">
+        <h2>Upload new model weights: </h2> 
+        <br />
+        <input className="upload"  onChange={handleFileInput} type="file" />
+        <br />
+        <button onClick={handleUpload}>Upload</button>
+      </div>
+
     </div>
   );
 }
